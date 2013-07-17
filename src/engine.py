c752c7be07907d275c0467cbe53f49b1a67e3993
abunai59 <abunai59@gmail.com>
2013-07-16 20:18:44 -1000

    Update code to draw a background image
    Current implementation draws the default image

--- src/engine.py
+++ src/engine.py
@@ -2,12 +2,20 @@
 # By:  abunai59
 
 import pygame
+import os
 
 # Define colors
 green = (   0,  255,    0)
+black = (   0,  0,      0)
 
 # Initialize the pygame library
 pygame.init()
+
+# Define path to executable
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+# Define path to resources
+__resources__ = __location__ + "\\..\\resources"
 
 # Define the window size
 size = [700,500]
@@ -15,6 +23,15 @@ size = [700,500]
 screen=pygame.display.set_mode(size)
 # Set the window title
 pygame.display.set_caption("Shashin Adventure")
+
+# Create a background surface to draw to
+background = pygame.Surface(screen.get_size())
+# Fill the background with black initially
+background.fill(black)
+# Define position of background image
+background_pos = [0,0]
+# Define initial background image
+background_img = pygame.image.load(__resources__ +"\default.png").convert()
 
 # Check for user quitting
 done = False
@@ -36,7 +53,7 @@ while done == False:
     #--Game logic
 
     #--Draw code
-    screen.fill(green)
+    screen.blit(background_img, background_pos)
 
     #--Update screen
     pygame.display.flip()
