b0eaf5771f74ab5994d12e87952bb5672ac45c24
abunai59 <abunai59@gmail.com>
2013-07-18 02:33:47 -1000

    Added imageLoader

---
+++ src/imageLoader.py
@@ -0,0 +1,43 @@
+# Image Loader
+# By: abunai59
+
+import glob
+
+class ImageLoader:
+    def __init__(self, res_path):
+        # Load image paths
+        pref = res_path + "\\*."
+        self.img_list = \
+            glob.glob(pref+"png") + \
+            glob.glob(pref+"jpg") + \
+            glob.glob(pref+"gif") + \
+            glob.glob(pref+"bmp") + \
+            glob.glob(pref+"pcx") + \
+            glob.glob(pref+"tga") + \
+            glob.glob(pref+"tif") + \
+            glob.glob(pref+"lbm") + \
+            glob.glob(pref+"pbm") + \
+            glob.glob(pref+"xpm")
+
+        # Current image is the first in the list
+        self.__current_index = 0
+
+    # Get the current image
+    def get_current(self):
+        return self.img_list[self.__current_index]
+
+    # Increment current index and return result
+    def get_next(self):
+        if (self.__current_index >= len(self.img_list)-1):
+            self.__current_index = 0
+        else:
+            self.__current_index += 1
+        return self.get_current()
+
+    # Decrement current index and return result
+    def get_prev(self):
+        if(self.__current_index <= 0):
+            self.__current_index = len(self.img_list)-1
+        else:
+            self.__current_index -= 1
+        return self.get_current()
